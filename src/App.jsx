afea06afa4b3b68a6871f0c404112b0d9561d8ad
MidShakes <106842736+MidShakes@users.noreply.github.com>
2022-07-04 01:56:53 -0300

    Add files via upload
    Questão 5 da atividade de recuperação de PW

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//App.jsx
+import './App.css'
+import List from './List.jsx'
+
+function App(){
+
+return(
+ //bloco   
+    <div>
+        <div className='text'>
+            
+
+        </div>
+
+        <List/>  
+    </div>
+)
+}
+export default App;
